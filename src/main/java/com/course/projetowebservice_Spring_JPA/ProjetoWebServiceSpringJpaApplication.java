982fa599040913f30f779fbbf0f0e92caff5163e
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-02-18 09:20:54 -0300

    Project created

---
+++ src/main/java/com/course/projetowebservice_Spring_JPA/ProjetoWebServiceSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.course.projetowebservice_Spring_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServiceSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServiceSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/projetowebservice_Spring_JPA/ProjetoWebServiceSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.projetowebservice_Spring_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServiceSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
